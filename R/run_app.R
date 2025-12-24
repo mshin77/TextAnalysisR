76b61e92506ad6463a4049b49b750841efa4d606
Mikyung Shin <shin.mikyung@gmail.com>
2025-12-23 20:16:59 -0600

    update

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,13 @@
-#' @title Launch and browse the TextAnalysisR app
+#' @title Launch the TextAnalysisR app
 #'
 #' @name run_app
 #'
 #' @description
-#' Launch and browse the TextAnalysisR app.
+#' Launch the TextAnalysisR Shiny application.
+#'
+#' @param launch.browser Logical. Whether to open the app in a browser.
+#'   Defaults to `interactive()`, which is FALSE in non-interactive sessions
+#'   (e.g., Docker containers, servers).
 #'
 #' @return No return value, called for side effects (launching Shiny app)
 #'
@@ -25,7 +29,7 @@
 #' @import widyr
 #' @importFrom magrittr %>%
 
-run_app <- function() {
+run_app <- function(launch.browser = interactive()) {
   appDir <- system.file("TextAnalysisR.app", package = "TextAnalysisR")
 
   if (appDir == "") {
@@ -53,5 +57,5 @@ run_app <- function() {
     }
   }
 
-  shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
+  shiny::runApp(appDir, display.mode = "normal", launch.browser = launch.browser)
 }
